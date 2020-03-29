d3a64c1aac63c7f8cecf1f062ad9ce504157ef61
Vitor Morelli <vitao.miacri@gmail.com>
2020-03-29 18:17:16 -0300

    Frontend Configuração Inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Routes from './routes';
+
+import './global.css';
+
+class App extends Component {
+  render() {
+    return <Routes />;
+  }
+}
+
+export default App;
